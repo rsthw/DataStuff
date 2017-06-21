74b9289888d8e072c9b1e3591d328a2299b061c4
Santiago Ley <santiago.slf@gmail.com>
2017-06-20 21:34:18 -0500

    Se agregan rutas, ejemplos y bootstrap

--- datastuff/__init__.py
+++ datastuff/__init__.py
@@ -7,6 +7,12 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
+
     config.add_route('home', '/')
+
+    config.add_route('layout', '/layout')
+
+    config.add_route('sample', '/sample')
+
     config.scan()
     return config.make_wsgi_app()
--- datastuff/views.py
+++ datastuff/views.py
@@ -4,3 +4,11 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='templates/mytemplate.jinja2')
 def my_view(request):
     return {'project': 'DataStuff'}
+
+@view_config(route_name='layout', renderer='templates/layout.jinja2')
+def layout_view(request):
+    return {'project': 'DataStuff'}
+
+@view_config(route_name='sample', renderer='templates/pageSample.jinja2')
+def sample_view(request):
+    return {'project': 'DataStuff'}
